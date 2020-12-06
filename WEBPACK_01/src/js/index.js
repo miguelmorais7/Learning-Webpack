4e0979b414e12b0ed9e024dd17eadb8c9dadc5c8
miguelmorais7 <42868106+miguelmorais7@users.noreply.github.com>
2020-12-06 14:10:06 -0300

    structural files update

---
+++ WEBPACK_01/src/js/index.js
@@ -0,0 +1,7 @@
+const textComponent = () =>{
+    const elH1 = document.createElement('h1');
+    elH1.innerHTML = 'Hello World!';
+    return elH1;
+}
+
+document.body.appendChild(textComponent());
